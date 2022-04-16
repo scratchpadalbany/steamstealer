101a5373b9dc9aa1a818ea5fb985ec171ba9922d
Denzel <info.albany@scratchpad.co.nz>
2022-04-16 12:51:12 +1200

    commit one

---
+++ script1.py
@@ -0,0 +1,16 @@
+import requests
+import json 
+key = "778CC62732B9E6FEC9E94295C4FB17C9"
+testid = "76561198127152089"
+test = requests.get("http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=778CC62732B9E6FEC9E94295C4FB17C9&steamid=76561198127152089&relationship=friend")
+test2 = test.json()
+print(test2)
+
+
+
+Get_Player_Achievements = requests.get("http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=960090&key=778CC62732B9E6FEC9E94295C4FB17C9&steamid=76561198053794848")
+print(Get_Player_Achievements)
+Get_Player_Achievements_2 = Get_Player_Achievements.text
+print(Get_Player_Achievements_2)
+file = open("test.json", "w")
+file.close()
