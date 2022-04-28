95493ae722d24fa4836870fd8e47e07aa4e5525d
scratchpadalbany <102392243+scratchpadalbany@users.noreply.github.com>
2022-04-28 16:59:58 +1200

    button that almost adds json elements

--- jsscript.js
+++ jsscript.js
@@ -3,6 +3,25 @@ let stringjson = JSON.stringify(jfile);
 let testjson = JSON.parse(stringjson);
 console.log("Test")
 console.log(testjson.friendslist.friends);
+console.log(String(testjson.friendslist.friends[0].steamid));
+let strid = String(testjson.friendslist.friends[0].steamid)
+
+/*
+bt1.addEventListener('click', event => {
+    test();
+  });
+
+function test(){
+    alert("The function 'test' is executed");
+    document.getElementById("friend1").innerHTML = str(testjson.friendslist.friends[1]); 
+    document.getElementById("friend1").innerHTML = "test2";
+}
+*/
+function test(){
+   
+    document.getElementById("friend1").innerHTML = strid; 
+}
+
 
 
 
