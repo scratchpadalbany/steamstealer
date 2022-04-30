f56307ecd4b2418b1ae73ea81bc93aff64f57566
scratchpadalbany <102392243+scratchpadalbany@users.noreply.github.com>
2022-04-30 13:04:54 +1200

    semi colons

--- scripts/jsscript.js
+++ scripts/jsscript.js
@@ -1,10 +1,10 @@
 const jfile = require("./test.json");
 let stringjson = JSON.stringify(jfile);
 let testjson = JSON.parse(stringjson);
-console.log("Test")
+console.log("Test");
 console.log(testjson.friendslist.friends);
 console.log(String(testjson.friendslist.friends[0].steamid));
-let strid = String(testjson.friendslist.friends[0].steamid)
+let strid = String(testjson.friendslist.friends[0].steamid);
 
 /*
 bt1.addEventListener('click', event => {
