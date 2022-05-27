609996e95b45e69bda37b334afce468e18aad5b7
Denzel <info.albany@scratchpad.co.nz>
2022-05-28 11:18:54 +1200

    avishai screw up

--- scripts/jsscript.js
+++ scripts/jsscript.js
@@ -1,84 +1,48 @@
-// Information to reach API
-const url = 'https://api.datamuse.com/words?sl=';
-
-// Selects page elements
-const inputField = document.querySelector('#input');
-const submit = document.querySelector('#submit');
-const responseField = document.querySelector('#responseField');
-
-// Asynchronous function
-const getSuggestions = () => {
-  const wordQuery = inputField.value;
-  const endpoint = `${url}${wordQuery}`;
-  
-  fetch(endpoint, {cache:' no-cache'}).then(response => {
+let theUrl = "http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=778CC62732B9E6FEC9E94295C4FB17C9&steamid=76561198127152089&relationship=friend";
+function callback(response) {
     if (response.ok) {
-      return response.json();
+        return response.json()
     }
-    throw new Error('Request failed!');
-  }, networkError => {
-    console.log(networkError.message)
-  }).then(jsonResponse => {
-    //renderRawResponse(jsonResponse)
-    renderResponse(jsonResponse);
-  })
+}
+function httpGetAsync(theUrl, callback(response))
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
 }
 
-// Clears previous results and display results to webpage
-const displaySuggestions = (event) => {
-  event.preventDefault();
-  while(responseField.firstChild){
-    responseField.removeChild(responseField.firstChild);
-  }
-  getSuggestions();
-};
+//const jfile = require("./test.json");
+let stringjson = JSON.stringify(jfile);
+let testjson = JSON.parse(stringjson);
+console.log("Test")
+console.log(testjson.friendslist.friends);
+console.log(String(testjson.friendslist.friends[0].steamid));
+let strid = String(testjson.friendslist.friends[0].steamid);
 
-submit.addEventListener('click', displaySuggestions);
-// Formats response to look presentable on webpage
-const renderResponse = (res) => {
-    // Handles if res is falsey
-    if(!res){
-      console.log(res.status);
-    }
-    // In case res comes back as a blank array
-    if(!res.length){
-      responseField.innerHTML = "<p>Try again!</p><p>There were no suggestions found!</p>";
-      return;
-    }
-  
-    // Creates an empty array to contain the HTML strings
-    let wordList = [];
-    // Loops through the response and caps off at 10
-    for(let i = 0; i < Math.min(res.length, 10); i++){
-      // creating a list of words
-      wordList.push(`<li>${res[i].word}</li>`);
-    }
-    // Joins the array of HTML strings into one string
-    wordList = wordList.join("");
-  
-    // Manipulates responseField to render the modified response
-    responseField.innerHTML = `<p>You might be interested in:</p><ol>${wordList}</ol>`;
-    return
-  }
-  
-  // Renders response before it is modified
-  const renderRawResponse = (res) => {
-    // Takes the first 10 words from res
-    let trimmedResponse = res.slice(0, 10);
-    // Manipulates responseField to render the unformatted response
-    responseField.innerHTML = `<text>${JSON.stringify(trimmedResponse)}</text>`;
-  }
-  
-  // Renders the JSON that was returned when the Promise from fetch resolves.
-  const renderJsonResponse = (res) => {
-    // Creates an empty object to store the JSON in key-value pairs
-    let rawJson = {};
-    for(let key in res){
-      rawJson[key] = res[key];
-    }
-    // Converts JSON into a string and adding line breaks to make it easier to read
-    rawJson = JSON.stringify(rawJson).replace(/,/g, ", \n");
-    // Manipulates responseField to show the returned JSON.
-    responseField.innerHTML = `<pre>${rawJson}</pre>`;
-  }
-  
+
+
+/*
+bt1.addEventListener('click', event => {
+    test();
+  });
+
+function test(){
+    alert("The function 'test' is executed");
+    document.getElementById("friend1").innerHTML = str(testjson.friendslist.friends[1]); 
+    document.getElementById("friend1").innerHTML = "test2";
+}
+*/
+function test(){
+   
+    document.getElementById("friend1").innerHTML = strid; 
+}
+
+
+
+
+
+
